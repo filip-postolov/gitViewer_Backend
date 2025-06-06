efaf8c1d42c47b97800eefa76f38353f3ffe4a26
Filip Postolov <postolovfilip@gmail.com>
2025-06-06 23:01:58 +0200

    Initial commit

---
+++ src/main/java/org/filippostolov/gitviewer/GitViewerBackendApplication.java
@@ -0,0 +1,13 @@
+package org.filippostolov.gitviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitViewerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitViewerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/filippostolov/gitviewer/GitViewerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.filippostolov.gitviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitViewerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
